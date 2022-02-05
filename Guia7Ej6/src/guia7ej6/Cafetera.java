322c34956c7574093d2507a7dc236aa9efcc9e46
Carola Nickel <carolamnickel@gmail.com>
2022-02-05 11:50:37 -0300

    ej6 arreglado y terminado

--- Guia7Ej6/src/guia7ej6/Cafetera.java
+++ Guia7Ej6/src/guia7ej6/Cafetera.java
@@ -1,8 +1,5 @@
 package guia7ej6;
 
-import java.util.Locale;
-import java.util.Scanner;
-
 //Atributos
 public class Cafetera {
 
@@ -15,8 +12,8 @@ public class Cafetera {
 
     //Repositorio parametrizado
     public void Cafetera(int capacidadMaxima, int cantidadActual) {
-        this.cantidadActual = cantidadActual;
-        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadActual = 0;
+        this.capacidadMaxima = 1000;
     }
 
     //SET - GET
@@ -36,6 +33,64 @@ public class Cafetera {
         this.cantidadActual = cantidadActual;
     }
 
-    
-    
+    //Metodo llenar cafetera. Hace que la cantidad actual sea igual a la capacidad máxima.
+    public void llenarCafetera() {
+        cantidadActual = 1000;
+    }
+
+    /* Metodo servirTaza(int): se pide el tamaño de una taza vacía, el método 
+    recibe el tamaño de la taza y simula la acción de servir la taza con la capacidad indicada. Si la
+    cantidad actual de café “no alcanza” para llenar la taza, se sirve lo que quede. El
+    método le informará al usuario si se llenó o no la taza, y de no haberse llenado en
+    cuanto quedó la taza. */
+    public void servirTaza(String opcion) {
+        int chico;
+        int mediano;
+        int grande;
+
+        switch (opcion) {
+            case "C":
+                System.out.println("Tamaño chico (60 ml)");
+
+                if (cantidadActual < 60) {
+                    System.out.println("Por favor, recargue la cafetera, el contenido que posee actualmente es insuficiente para completar el pedido");
+                } else {
+                    chico = (cantidadActual = cantidadActual - 60);
+                    System.out.println("Quedan " + cantidadActual + " ml en la cafetera");
+                }
+                break;
+            case "M":
+                System.out.println("Tamaño mediano(200 ml)");
+                if (cantidadActual < 200) {
+                    System.out.println("Por favor, recargue la cafetera, el contenido que posee actualmente es insuficiente para completar el pedido");
+                } else {
+                    mediano = (cantidadActual = cantidadActual - 200);
+                    System.out.println("Quedan " + cantidadActual + " ml en la cafetera");
+                }
+                break;
+            case "G":
+                System.out.println("Tamaño grande (300 ml)");
+                if (cantidadActual < 300) {
+                    System.out.println("Por favor, recargue la cafetera, el contenido que posee actualmente es insuficiente para completar el pedido");
+                } else {
+                    grande = (cantidadActual = cantidadActual - 300);
+                    System.out.println("Quedan " + cantidadActual + " ml en la cafetera");
+                }
+                break;
+        }
+    }
+
+    //metodo para vaciar cafetera
+    public void vaciarCafetera() {
+        cantidadActual = 0;
+        System.out.println("La cafetera fue vaciada");
+    }
+    //llenado progresivo de cafetera
+
+    public void agregarCafe(int cantidad) {
+        cantidadActual = cantidadActual + cantidad;
+        System.out.println("Se recargo la cafetera a " + cantidadActual + " ml");
+
+    }
+
 }
--- Guia7Ej6/src/guia7ej6/Guia7Ej6.java
+++ Guia7Ej6/src/guia7ej6/Guia7Ej6.java
@@ -19,11 +19,56 @@ recibe y se añade a la cafetera la cantidad de café indicada
  */
 package guia7ej6;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Guia7Ej6 {
 
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in, "ISO-8859-1").useDelimiter("\n").useLocale(Locale.US);
+        String opcion;
+        int cantidad;
+        int seleccion;
 
-        
+        Cafetera cafetera = new Cafetera();
+        System.out.println("Bienvenido a Resto Cafeteria");
+
+        do {
+            System.out.println(" ");
+            System.out.println("Por favor seleccione la opcion con la cual desearia continuar ");
+            System.out.println("1) Llenar cafetera ");
+            System.out.println("2) Seleccion del tamaño del cafe ");
+            System.out.println("3) Vaciar la cafetera ");
+            System.out.println("4) Para ingresar manualmente cuanto cafe agregar a la cafetera");
+            System.out.println("5) Salir");
+
+            seleccion = leer.nextInt();
+            switch (seleccion) {
+                case 1:
+                    cafetera.llenarCafetera();
+                    System.out.println("Cafetera llena ");
+                    break;
+                case 2:
+                    //Metodo de servir taza, eleccion
+                    System.out.println("Por favor, seleccione el tamaño del cafe que desea ");
+                    System.out.println("Poseemos 3 tamaños: Grande, Mediano y Chico");
+                    System.out.println("Para seleccionar el tamaño ingrese la inicial que corresponda (G, M, C)");
+                    opcion = leer.next();
+                    cafetera.servirTaza(opcion);
+
+                    break;
+                case 3:
+                    cafetera.vaciarCafetera();
+                    break;
+                case 4:
+                    //metodo agregarCafe
+                    System.out.println("Seleccione la cantidad de ml que desea cargar, recuerde que la cafetera tiene una capacidad maxima de 1000 ml");
+                    cantidad = leer.nextInt();
+                    cafetera.agregarCafe(cantidad);
+            }
+
+        } while (seleccion != 5);
+        System.out.println("Gracias por su compra, sesion cerrada");
     }
-    
+
 }
